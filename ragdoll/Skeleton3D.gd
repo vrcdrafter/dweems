4b155b34f7a540717b5d337cf80d70893f2a4fb7
Mat Hawley <102326953+vrcdrafter@users.noreply.github.com>
2023-09-07 22:11:14 -0400

    added correct constraints .

--- ragdoll/Skeleton3D.gd
+++ ragdoll/Skeleton3D.gd
@@ -1,10 +1,10 @@
 extends Skeleton3D
 
-var array = ["Spine.001","Spine.002","Spine003","Spine004"]
+var array = ["Spine001","Spine002","Spine003","Spine004"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	physical_bones_start_simulation(array)
-	pass
+	#pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
